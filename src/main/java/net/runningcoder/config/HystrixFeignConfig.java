f776f71bdda814c020263530eaef6ea4b83ee160
moc <758548361@qq.com>
2017-11-10 11:01:23 +0800

    添加feign日志

--- src/main/java/net/runningcoder/config/HystrixFeignConfig.java
+++ src/main/java/net/runningcoder/config/HystrixFeignConfig.java
@@ -1,10 +1,12 @@
 package net.runningcoder.config;
 
 import com.netflix.hystrix.HystrixCommandProperties;
+import feign.Logger;
 import feign.hystrix.HystrixFeign;
 import feign.hystrix.SetterFactory;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.slf4j.Slf4jLogger;
 import net.runningcoder.remote.LocalApi;
 import net.runningcoder.remote.LocalApiFallback;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +22,7 @@ public class HystrixFeignConfig {
         return HystrixFeign.builder()
                 .decoder(new JacksonDecoder())
                 .encoder(new JacksonEncoder())
+                .logger(new Slf4jLogger(LocalApi.class)).logLevel(Logger.Level.FULL)
                 .setterFactory((target, method) ->
                         new SetterFactory.Default().create(target, method).andCommandPropertiesDefaults(
                                 HystrixCommandProperties.defaultSetter()
